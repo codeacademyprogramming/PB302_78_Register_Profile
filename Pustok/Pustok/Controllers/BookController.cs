e1c1e9d122408f0144d148036d5fa192c1358e24
Elchin Aghayev <Elchin@code.local>
2024-05-08 21:42:19 +0400

    project added

---
+++ Pustok/Pustok/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.Models;
+
+namespace Pustok.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly PustokDbContext _context;
+        private readonly CountService countService;
+        private readonly CountManageService countManageService;
+
+        public BookController(PustokDbContext context, CountService countService, CountManageService countManageService)
+        {
+            _context = context;
+            this.countService = countService;
+            this.countManageService = countManageService;
+        }
+        public IActionResult GetBookById(int id)
+        {
+            Book book = _context.Books.Include(x => x.Genre).Include(x=>x.BookImages.Where(x=>x.Status==true)).FirstOrDefault(x => x.Id == id);
+            return PartialView("_BookModalPartial",book);
+        }
+
+
+        //service lifecycle
+        public IActionResult Add()
+        {
+            countService.Add();
+            countService.Add();
+            countService.Add();
+
+            countManageService.Add();
+            countManageService.Add();
+
+
+            return Json(new { count = countService.Count });
+        }
+
+
+
+    }
+}
